5ce94176dd107c069c10d72adcd8c93644cff947
mthoko-max29 <mthokozisimpala247@gmail.com>
2025-09-17 11:39:12 +0200

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,13 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> # Constant
+... PI = 3.14159
+... 
+... # Variable
+... radius = 5
+... area = PI * radius ** 2
+... 
+... # Output using print()
+... print("Radius of the circle:", radius)
+... print("Area of the circle:", area)
+... 
